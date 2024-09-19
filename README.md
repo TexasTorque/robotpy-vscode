b7db77b83bba58a02cc2394edadc79219ac56e51
humandavey <davey.adams.three@gmail.com>
2024-09-19 16:43:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RobotPy
+
+The Unofficial RobotPy Extension is used for easy deployment, syncing, and simulating of your robotpy projects
---
+++ src/extension.ts
@@ -0,0 +1,37 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	let terminal: vscode.Terminal = vscode.window.createTerminal("RobotPy");
+
+	let sync = vscode.commands.registerCommand('robotpy.sync', () => {
+		vscode.window.showInformationMessage('RobotPy Sync');
+
+		terminal.show();
+		terminal.sendText("robotpy sync");
+	});
+
+	let deploy = vscode.commands.registerCommand('robotpy.deploy', () => {
+		vscode.window.showInformationMessage('RobotPy Deploy');
+
+		terminal.show();
+		terminal.sendText("robotpy deploy --skip-tests");
+	});
+
+	let simulate = vscode.commands.registerCommand('robotpy.simulate', () => {
+		vscode.window.showInformationMessage('RobotPy Simulate');
+
+		terminal.show();
+		terminal.sendText("robotpy sim");
+	});
+
+	context.subscriptions.push(sync);
+	context.subscriptions.push(deploy);
+	context.subscriptions.push(simulate);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
